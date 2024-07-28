caf9d337575a0ba93c28c7cf5ac3a2cdb7828949
Mr Devil <168351611+mrdevil42023@users.noreply.github.com>
2024-07-28 21:11:16 +0530

    Update start.py

--- Devil/start.py
+++ Devil/start.py
@@ -8,27 +8,23 @@ from pyrogram import Client, filters
 
 
 joinButton = InlineKeyboardMarkup([
-    [
-        InlineKeyboardButton(text="😈 𝙾𝙽𝚆𝙴𝚁 😈",url="http://t.me/mrdevil12"
-        ),
-    ],
-    [
-        InlineKeyboardButton(text="𝚄𝙿𝙳𝙰𝚃𝙴 𝙲𝙷𝙰𝙽𝙽𝙴𝙻", url="https://t.me/devilbots971"),
-        InlineKeyboardButton(text="𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝙶𝚁𝙾𝚄𝙿", url="https://t.me/devilbotsupport"),
-    ],
-    [
-        InlineKeyboardButton(text="𝙲𝙷𝙰𝚃 𝙶𝚁𝙾𝚄𝙿",= url="https://t.me/akatsuki976"),
-    ])
+    [InlineKeyboardButton(
+        "🚀『 𝐆𝐫𝐨𝐮𝐩 』",
+        url="https://t.me/Krakns")],
+    [InlineKeyboardButton(
+        "🍤『 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 』",
+        url="https://t.me/KrakinzLab")],
+])
 
 
 @Client.on_message(filters.command("start"))
 async def start(_, ryui: Message):
     user_and_chats = ryui.from_user.first_name
     await ryui.reply_photo(
-        "https://graph.org/file/5184a670042b456f1085f.jpg",
+        "https://telegra.ph/file/e26f9a6f0082b4171b6ef.jpg",
         reply_markup=joinButton,
-        caption=f"""𝐇𝐞𝐥𝐥𝐨 𝐈𝐚𝐦 𝐃𝐄𝐕𝐈𝐋 𝐗 𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐏𝐇
-            𝐇𝐎𝐖 𝐀𝐑𝐄 𝐘𝐎𝐔 **__`{user_and_chats}`__**,
+        caption=f"""╰☆☆••| 𝗜𝗺𝗮𝗴𝗲 𝟮 𝗨𝗥𝗟 |••☆☆╮
+            𝐇𝐨𝐰𝐝𝐲 **__`{user_and_chats}`__**,
            
 🏷 ɪᴍᴀɢᴇ ᴛᴏ ᴜʀʟ ʙᴏᴛ ᴄᴀɴ ᴄᴏɴᴠᴇʀᴛ ᴛʜᴇꜱᴇ ʙᴇʟᴏᴡ ꜰɪʟᴇ ᴛʏᴘᴇꜱ ᴛᴏ [ᴛᴇʟᴇɢʀᴀᴘʜ](https://pypi.org/project/telegraph/) ᴜʀʟ.
 🏷 ᴊᴜꜱᴛ ꜱᴇɴᴅ ᴘʜᴏᴛᴏ ᴇɪᴛʜᴇʀ ɪɴ ᴄᴏᴍᴘʀᴇꜱꜱᴇᴅ ᴏʀ ᴜɴᴄᴏᴍᴘʀᴇꜱꜱᴇᴅ ꜰᴏʀᴍᴀᴛ
@@ -42,5 +38,6 @@ async def start(_, ryui: Message):
 🏷 ꜰɪʟᴇꜱ ᴍᴏʀᴇ ᴛʜᴇɴ 5ᴍʙ ɪꜱ ɴᴏᴛ ꜱᴜᴘᴘᴏʀᴛᴇᴅ ʙʏ ᴛᴇʟᴇɢʀᴀᴘʜ.
 
 
-🖥ᴛʜɪꜱ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ @mrdevil12 | ꜰᴏʀ ꜱᴜᴘᴘᴏʀᴛ ᴘʟᴇᴀꜱᴇ ᴊᴏɪɴ ᴏᴜʀ ɢʀᴏᴜᴘ @devilbotsupport""")
+🖥 Dҽʋ Mҽɳƚισɳ: @Krakinz | @KrakinzBot
+╰☆☆••| 𝗜𝗺𝗮𝗴𝗲 𝟮 𝗨𝗥𝗟 |••☆☆╮""")
     raise StopPropagation
